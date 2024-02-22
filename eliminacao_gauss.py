18a90b25af681cfc2014e421b2e2b671f0d97a85
Murilo <lilo.marino@hotmail.com>
2024-02-21 21:27:27 -0300

    Melhorado visualização de eliminação de gauss

--- eliminacao_gauss.py
+++ eliminacao_gauss.py
@@ -2,15 +2,17 @@ import pandas as pd
 import numpy as np
 
 # Carrega a matriz a partir de um arquivo Excel
-matriz = pd.read_excel('matriz.xlsx', header=None).values
+matriz = pd.read_excel("matriz.xlsx", header=None).values
 
 # Converte as colunas para o tipo de dados float
 matriz = matriz.astype(float)
 
+
 # Função para realizar a eliminação de Gauss com pivotamento
-def eliminacao_gauss_pivotamento(matriz):
+def eliminacao_gauss_pivotamento(matriz: np.ndarray):
     n = len(matriz)
     for i in range(n):
+        print(f"Passo {i+1}:")
         # Encontra o pivô para a coluna atual
         max_index = i
         for j in range(i + 1, n):
@@ -19,11 +21,17 @@ def eliminacao_gauss_pivotamento(matriz):
         # Troca as linhas se necessário
         if max_index != i:
             matriz[[i, max_index]] = matriz[[max_index, i]]
+
+        for idx in range(i + 1):
+            print(matriz[:, idx])
         # Eliminação de Gauss
         for j in range(i + 1, n):
             fator = matriz[j, i] / matriz[i, i]
-            matriz[j, i:] -= fator * matriz[i, i:]
+            print(f"{matriz[j]} - {fator} * L{i+1}")
+            matriz[j] -= fator * matriz[i]
+
     return matriz
+
 
 # Realiza a eliminação de Gauss com pivotamento
 matriz_resultante = eliminacao_gauss_pivotamento(matriz)
@@ -31,8 +39,10 @@ matriz_resultante = eliminacao_gauss_pivotamento(matriz)
 # Resolução do sistema
 x = np.zeros(len(matriz_resultante))
 for i in range(len(matriz_resultante) - 1, -1, -1):
-    x[i] = (matriz_resultante[i, -1] - np.dot(matriz_resultante[i, i+1:-1], x[i+1:])) / matriz_resultante[i, i]
+    x[i] = (
+        matriz_resultante[i, -1] - np.dot(matriz_resultante[i, i + 1 : -1], x[i + 1 :])
+    ) / matriz_resultante[i, i]
 
 # Imprime a solução
 print("Solução:")
-print(x)
+print(x.view())
