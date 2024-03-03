1dc3fa0d6db721275f5ee36e7619db4e12384411
Murilo <lilo.marino@hotmail.com>
2024-03-03 14:05:30 -0300

    Corrigido gauss_seidel()

--- gauss_iterations.py
+++ gauss_iterations.py
@@ -5,13 +5,15 @@ import pandas as pd
 def gauss_jacobi(A: np.ndarray, B: np.ndarray, iterations: int):
     n = len(A)
     X = np.zeros(n, dtype=float)
+    X_OLD = np.zeros(n, dtype=float)
     for k in range(0, iterations + 1):
         print(f"X{k} = {X}")
         for i in range(n):
             somatorio = 0
             for j in range(n):
                 if j != i:
-                    somatorio += A[i,j]*X[j]
+                    somatorio += A[i,j]*X_OLD[j]
+            X_OLD[i] = X[i]
             X[i] = (B[i,0] - somatorio)/A[i,i]
     return X
 
